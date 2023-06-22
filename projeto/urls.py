bc96f9ce90c27aa96483a3aadc58fd85b14d8fa9
Reiner Santiago Silva <reiner.santyago@gmail.com>
2023-06-22 16:26:36 -0300

    general update

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import home, teste
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", teste),
+    path("home/", home),
 ]
